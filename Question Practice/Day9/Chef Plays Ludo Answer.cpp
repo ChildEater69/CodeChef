23a53422ee7552f753c96cc331d242717fdf0fa7
Avdhesh Kumar Sharma <115105709+ChildEater69@users.noreply.github.com>
2023-06-12 20:47:33 +0530

    s

---
+++ Question Practice/Day9/Chef Plays Ludo Answer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,a;
+	cin>>t;
+	while(t--){
+	    cin>>a;
+	    if(a==6){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
