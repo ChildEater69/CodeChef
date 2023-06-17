5d410c050a7be2e096eab902efcfcc1e0cf0c8b4
Avdhesh Kumar Sharma <115105709+ChildEater69@users.noreply.github.com>
2023-06-17 22:24:33 +0530

    d

---
+++ Question Practice/Day14/Mario and Bullet/Mario and Bullet Answer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int x,y,z;
+		cin>>x>>y>>z;
+		
+		if(z>(y/x))
+		{
+			cout<<z-(y/x)<<endl;
+		}
+		else
+			cout<<0<<endl;
+	}
+	return 0;
+}
