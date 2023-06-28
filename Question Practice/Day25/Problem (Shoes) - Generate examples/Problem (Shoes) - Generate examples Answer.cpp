7d3d4f4d8e2ec27f8020c7d8bba87f4fec51e220
Avdhesh Kumar Sharma <115105709+ChildEater69@users.noreply.github.com>
2023-06-28 19:29:36 +0530

    d

---
+++ Question Practice/Day25/Problem (Shoes) - Generate examples/Problem (Shoes) - Generate examples Answer.cpp
@@ -0,0 +1,9 @@
+Select correct options from the following.
+For N = 10 and M = 10, the number of extra shoes that Chef has to buy is 10.
+For N = 10 and M = 10, the number of extra shoes that Chef has to buy is 20.
+For N = 5 and M = 10, the number of extra shoes that Chef has to buy is 5.
+For N = 5 and M = 10, the number of extra shoes that Chef has to buy is 10.
+For N = 15 and M = 10, the number of extra shoes that Chef has to buy is 20.
+For N = 15 and M = 10, the number of extra shoes that Chef has to buy is 15.
+Status:
+Hooray, you did it!
