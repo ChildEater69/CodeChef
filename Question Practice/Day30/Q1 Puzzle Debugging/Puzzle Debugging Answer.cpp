ddecea26736dd9482a4e40631c35e8d915c6bcd5
Avdhesh Kumar Sharma <115105709+ChildEater69@users.noreply.github.com>
2023-07-03 11:09:56 +0530

    a

---
+++ Question Practice/Day30/Q1 Puzzle Debugging/Puzzle Debugging Answer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+                              // your code goes here
+{   int  t,n,x;
+
+    cin>>t;
+    
+    while(t--)
+    {
+        cin>>n>>x;
+        
+        cout<<(2*n)-x+1<<endl;
+    }
+	return 0;
+}
