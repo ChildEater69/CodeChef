f31b95aaadface2b68f5c0091767006beec9f014
Avdhesh Kumar Sharma <115105709+ChildEater69@users.noreply.github.com>
2023-07-15 10:47:14 +0530

    new

---
+++ Question Practice/Day42/Who is taller! Answer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int T;
+   cin>>T;
+   while(T--)
+   {
+       int X,Y;
+       cin>>X>>Y;
+       if(X>Y)
+       cout<<"A"<<endl;
+       else
+       cout<<"B"<<endl;
+   }
+	return 0;
+}
